ffd0236e535a5d18754ad9f5ad54fb1e32b9b6ff
syed-naseer-abbas <66269007+syed-naseer-abbas@users.noreply.github.com>
2021-09-17 23:19:58 +0500

    Update bio.js
    Replace Twitter

--- src/components/bio.js
+++ src/components/bio.js
@@ -55,8 +55,8 @@ const Bio = () => {
         <p>
           Written by <strong>{author.name}</strong> {author?.summary || null}
           {` `}
-          <a href={`https://twitter.com/${social?.twitter || ``}`}>
-            Follow on Twitter
+          <a href={`https://youtube.com/channel/UCc7xxMNhp1TklKpcc34gROA?sub_confirmation=1`}>
+            Follow on YouTube
           </a>
         </p>
       )}
